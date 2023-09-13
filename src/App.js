7fa6f1ad1593f3622b5a8e22e513e49b6d3ed212
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-09-13 15:27:06 +0700

    sa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Assads
+          Learn React Assads dsadas
         </a>
       </header>
     </div>
