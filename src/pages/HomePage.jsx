d0ef76b4aaad32081c227d5ef136c52a8f0a83b0
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-09-19 14:07:00 +0700

    dsa

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -151,29 +151,22 @@ export default function HomePage() {
           <form
             name="contact"
             method="POST"
-            data-netlify-recaptcha="true"
-            netlify-honeypot="bot-field"
-            netlify
             data-netlify="true"
+            data-netlify-recaptcha="true"
           >
-            <label>
-              Don’t fill this out if you’re human: <input name="bot-field" />
-            </label>
+            <input type="hidden" name="form-name" value="contact" />
             <p>
               <label>
-                {" "}
-                Your Name: <input type="text" name="name" />{" "}
+                Your Name: <input type="text" name="name" />
               </label>
             </p>
             <p>
               <label>
-                {" "}
-                Email: <input type="text" name="email" />{" "}
+                Your Email: <input type="email" name="email" />
               </label>
             </p>
             <p>
               <label>
-                {" "}
                 Message: <textarea name="message"></textarea>
               </label>
             </p>
@@ -181,7 +174,6 @@ export default function HomePage() {
             <p>
               <button type="submit">Send</button>
             </p>
-            <input type="hidden" name="form-name" value="contact" />
           </form>
           <Center>
             <Button
